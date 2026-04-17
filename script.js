8f8044fddecb8f3f56708b5801f51c320c44fd0a
DawidKumor <dawidkumor@gmail.com>
2026-04-17 20:21:33 +0200

    Add live Markdown preview with regex-based converter

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const markdownInput = document.getElementById("markdown-input");
 const htmlOutput = document.getElementById("html-output");
+const preview = document.getElementById("preview");
 function convertMarkdown() {
     let str = ""
     const regexH1 = /^# (?<h1>.+)/gim;
     const regexH2 = /^## (?<h2>.+)/gim;
     const regexH3 = /^### (?<h3>.+)/gim;
+    const regexBold = /\*\*(?<B>.+)\*\*|__(?<B>.+)__/gim;
+    const regexItalic = /\*(?<I>.+)\*|_(?<I>.+)_/gim;
+    const regexImg = /!\[(?<alt>.+)\]\((?<img>.+)\)/gim;
+    const regexUrl = /(?<!!)\[(?<l>.+)\]\((?<url>.+)\)/gim;
+    const regexQuote = /^> (?<q>.+)/gim;
     str = markdownInput.value.replaceAll(regexH1, "<h1>$<h1></h1>").replaceAll(regexH2, "<h2>$<h2></h2>")
-    .replace(regexH3, "<h3>$<h3></h3>");
+        .replaceAll(regexH3, "<h3>$<h3></h3>").replaceAll(regexQuote, "<blockquote>$<q></blockquote>")
+        .replaceAll(regexBold, "<strong>$<B></strong>").replaceAll(regexItalic, "<em>$<I></em>")
+        .replaceAll(regexImg, '<img alt="$<alt>" src="$<img>">').replaceAll(regexUrl, '<a href="$<url>">$<l></a>');
     return str;
 }
 markdownInput.addEventListener("input", () => {
     htmlOutput.innerText = convertMarkdown();
+    preview.innerHTML = convertMarkdown()
 });
 markdownInput.addEventListener("focus", () => {
     markdownInput.classList.add("background");
