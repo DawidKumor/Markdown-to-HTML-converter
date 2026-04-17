ebf2bd6a1f2a923f9458bdb10a633447fff61624
DawidKumor <dawidkumor@gmail.com>
2026-04-17 18:48:18 +0200

    eventListeners added

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const markdownInput = document.getElementById("markdown-input");
+const htmlOutput = document.getElementById("html-output");
 function convertMarkdown() {
-let str = ""
-  const regexH1 = /^# (?<h1>.+)/gim;
-  const regexH2 = /^## (?<h2>.+)/gim;
-  const regexH3 = /^### (?<h3>.+)/gim;
-  str = markdownInput.value.replaceAll(regexH1, "<h1>$<h1></h1>").replaceAll(regexH2, "<h1>$<h2></h1>")
-  return str;
+    let str = ""
+    const regexH1 = /^# (?<h1>.+)/gim;
+    const regexH2 = /^## (?<h2>.+)/gim;
+    const regexH3 = /^### (?<h3>.+)/gim;
+    str = markdownInput.value.replaceAll(regexH1, "<h1>$<h1></h1>").replaceAll(regexH2, "<h2>$<h2></h2>")
+    .replace(regexH3, "<h3>$<h3></h3>");
+    return str;
 }
+markdownInput.addEventListener("input", () => {
+    htmlOutput.innerText = convertMarkdown();
+});
+markdownInput.addEventListener("focus", () => {
+    markdownInput.classList.add("background");
+});
+markdownInput.addEventListener("blur", () => {
+    markdownInput.classList.remove("background");
+})
